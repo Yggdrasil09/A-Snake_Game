3950ac3ae2e2dcbfc51b04b193d11bcdf1bf6aa2
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-10-17 23:48:15 +0530

    Create a Base work for snake game

---
+++ snake.js
@@ -0,0 +1,11 @@
+function setup()
+{
+    var canvas1=createCanvas(600, 600);
+    background(0, 0, 0);
+    canvas1.position((windowWidth - width) / 2,(windowHeight - height) / 2);
+}
+
+function draw()
+{
+    rect(100,200,60,70);
+}
