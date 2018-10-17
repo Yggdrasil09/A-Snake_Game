f1003309dbd1d019531d4452f77b8a9aefc9c5da
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-10-18 01:21:43 +0530

    Added the motion to the body

--- snake.js
+++ snake.js
@@ -1,11 +1,49 @@
+var x=0,y=0;
+
+var dir=0;
+
 function setup()
 {
-    var canvas1=createCanvas(600, 600);
+    var canvas=createCanvas(600, 600);
     background(0, 0, 0);
-    canvas1.position((windowWidth - width) / 2,(windowHeight - height) / 2);
+    canvas.position((windowWidth - width) / 2,(windowHeight - height) / 2);
 }
 
 function draw()
 {
-    rect(100,200,60,70);
+    rect(x,y,10,10);
+    switch(dir)
+    {
+        case 0:
+                y++;
+                break;
+        case 1:
+                y--;
+                break;
+        case 2:
+                x++;
+                break;
+        case 3:
+                x--;
+    }
+}
+
+function keyTyped()
+{
+    if(key=='s'||key=='S')
+    {
+        dir=0;
+    }
+    else if(key=='w'||key=='W')
+    {
+        dir=1;
+    }
+    else if(key=='d'||key=='D')
+    {
+        dir=2;
+    }
+    else if(key=='a'||key=='A')
+    {
+        dir=3;
+    }
 }
