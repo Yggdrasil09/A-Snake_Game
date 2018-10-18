eef7b2549e787cb59f34e5213158d27d7ab0e70a
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-10-18 17:12:56 +0530

    Building of the body is done

--- snake.js
+++ snake.js
@@ -12,7 +12,15 @@ function setup()
     frameRate(40);
     snk=new Snake();
     frameRate(10);
-    food=createVector(random(width),random(height));
+    relocat();
+}
+
+function relocat()
+{
+    var cols=floor(width/slength);
+    var rows=floor(height/slength);
+    food=createVector(floor(random(cols)),floor(random(rows)));
+    food.mult(slength);
 }
 
 function draw()
@@ -20,6 +28,10 @@ function draw()
     background(0, 0, 0);
     snk.update();
     snk.show();
+    if(snk.eatfood(food))
+    {
+        relocat();
+    }
     fill(230,30,230);
     rect(food.x,food.y,slength,slength);
 }
@@ -50,22 +62,50 @@ function Snake()
     this.y=0;
     this.xSpeed=1;
     this.ySpeed=0;
+    this.total=0;
+    this.tail=[];
     this.direc=function(x,y)
     {
         this.xSpeed=x;
         this.ySpeed=y;
     };
+    this.eatfood=function(pos)
+    {
+        var d=dist(this.x,this.y,pos.x,pos.y);
+        if(d<1)
+        {
+            this.total++;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    };
     this.update=function()
     {
+        if(this.total===this.tail.length)
+        {
+            for(var i=0;i<this.tail.length-1;i++)
+            {
+                this.tail[i]=this.tail[i+1];
+            }
+        }
+        this.tail[this.total-1]=createVector(this.x,this.y);
         this.x+=this.xSpeed*slength;
         this.y+=this.ySpeed*slength;
 
         this.x=constrain(this.x,0,width-slength);
         this.y=constrain(this.y,0,height-slength);
+
     };
     this.show=function()
     {
         fill(255,255,255);
+        for(var i=0;i<this.total;i++)
+        {
+            rect(this.tail[i].x,this.tail[i].y,slength,slength);
+        }
         rect(this.x,this.y,slength,slength);
     };
 }
